2c07dbcb3ac266e97de7a38c484c9164e1ded75a
Alexey_Makeev <makeevhome@mail.ru>
2023-10-24 23:18:57 +0300

    up step

--- apps/users/src/app/config/app-environment.ts
+++ apps/users/src/app/config/app-environment.ts
@@ -6,11 +6,14 @@ const MAX_PORT = 65535;
 export enum EnvValidationMessage {
   EnvironmentRequired = 'environment is required',
   PortRequired = 'port is required',
+  ServeRootRequired = 'Serve root is required',
+  HostRequired = 'Host is required',
+  UploadDirectoryRequired = 'Upload directory is required',
 }
 
 export class AppEnvironment {
   @IsNumber(
-    {},
+    { maxDecimalPlaces: MAX_PORT },
     {
       message: EnvValidationMessage.PortRequired,
     },
@@ -23,4 +26,19 @@ export class AppEnvironment {
     message: EnvValidationMessage.EnvironmentRequired,
   })
   public environment: string;
+
+  @IsString({
+    message: EnvValidationMessage.ServeRootRequired,
+  })
+  public serveRoot: string;
+
+  @IsString({
+    message: EnvValidationMessage.HostRequired,
+  })
+  public host: string;
+
+  @IsString({
+    message: EnvValidationMessage.UploadDirectoryRequired,
+  })
+  public uploadDirectory: string;
 }
--- apps/users/src/app/config/app.config.ts
+++ apps/users/src/app/config/app.config.ts
@@ -3,17 +3,23 @@ import { validateSync } from 'class-validator';
 import { AppEnvironment } from './app-environment';
 import { plainToInstance } from 'class-transformer';
 
-const DEFAULT_PORT = 3000;
+const DEFAULT_PORT = 4000;
 
 export interface ApplicationConfig {
   environment: string;
   port: number;
+  host: string;
+  serveRoot: string;
+  uploadDirectory: string;
 }
 
 export default registerAs('application', (): ApplicationConfig => {
   const config: ApplicationConfig = {
-    environment: process.env['NODE_ENV'] || 'development',
-    port: parseInt(process.env['PORT'] || DEFAULT_PORT.toString(), 10),
+    environment: process.env.NODE_ENV || 'development',
+    port: parseInt(process.env.PORT || DEFAULT_PORT.toString(), 10),
+    host: process.env.HOST || 'http://localhost',
+    serveRoot: process.env.SERVE_ROOT || '/static',
+    uploadDirectory: process.env.UPLOAD_DIRECTORY_PATH || '/uploads',
   };
   const appEnvironment = plainToInstance(AppEnvironment, config, { enableImplicitConversion: true });
 
