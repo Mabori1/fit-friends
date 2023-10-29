3f33d1ecdb65543934a2a15fc3828d7a00f2e28a
Alexey_Makeev <makeevhome@mail.ru>
2023-10-29 20:44:45 +0300

    fix seed db

--- prisma/migrations/20231027115055_init4/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `total_price` on the `orders` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "orders" DROP COLUMN "total_price",
-ADD COLUMN     "sum_price" INTEGER NOT NULL DEFAULT 0;
